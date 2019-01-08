562d83546a13ae1474b59919d0e022abafc8c2c3
Robert Pruden <rdp1070@rit.edu>
2019-01-07 21:39:30 -0500

    Add some wonky animation to the StatPanel, start setting up the stat work

---
+++ Assets/InfoPanelController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoPanelController : MonoBehaviour {
+
+    public List<Stat> stats;
+    public FruitGeneticsController statInfo;
+    public string babyName;
+    public CanvasGroup canvasGroup;
+
+	// Use this for initialization
+	void Start () {
+        PanelInit();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // if you are looking at the baby, fade in the stats screen;
+        
+	}
+
+    public void ShowSelf() {
+        // do an animation where you fade in
+        var imgs = canvasGroup.GetComponentsInChildren<Image>();
+           foreach (Image img in imgs)
+            img.CrossFadeAlpha(1, 2f, false);
+    }
+
+    public void HideSelf() {
+        // do an animation where you fade out
+        print("Hide Self");
+        var imgs = canvasGroup.GetComponentsInChildren<Image>();
+        foreach (Image img in imgs)
+            img.CrossFadeAlpha(0, 2f, false);
+        
+    }
+
+    public void PanelInit() {
+        // initialize all the values for the panel
+        HideSelf();
+
+        stats = new List<Stat>();
+        babyName = statInfo.name ?? "Test";
+
+        foreach (Stat stat in stats) {
+            // make a new UI element representing the stat
+            // and fill the info in with the stat info
+        }
+    }
+}
--- Assets/Scripts/FruitGeneticsController.cs
+++ Assets/Scripts/FruitGeneticsController.cs
@@ -4,22 +4,68 @@ using UnityEngine;
 
 public class FruitGeneticsController : MonoBehaviour {
 
+    public GameObject statPanel;
 
-    public int stat_total;
+    public List<Stat> stats;
 
-    enum MyEnum
-    {
-
-    }
-
+    public Stat FStat;
+    public Stat RStat;
+    public Stat UStat;
+    public Stat IStat;
+    public Stat TStat;
 
 	// Use this for initialization
 	void Start () {
-		
+
+        stats = new List<Stat>(){ FStat, RStat, UStat, IStat, TStat };
+
+        if (statPanel == null) {
+            GameObject.FindGameObjectsWithTag("Stat Panel");
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+}
+
+public class Stat {
+
+
+    public Stat(string Name) {
+        name = Name;
+        growthRate = 1;
+        value = 0;
+        baseValue = 0;
+        potentialValue = 100;
+    }
+
+    /// <summary>
+    /// The name of the stat that appears in the label
+    /// </summary>
+    public string name;
+    /// <summary>
+    /// Everytime you do an activity it adds a multiplier 
+    /// times your growthRate to the value of the stat. 
+    /// This number indicates how fast it grows.
+    /// </summary>
+    public float growthRate;
+    /// <summary>
+    /// Current value of the stat.
+    /// </summary>
+    public float value;
+    /// <summary>
+    /// The stat level the baby is born with.
+    /// </summary>
+    public float baseValue;
+    /// <summary>
+    /// The stat maximum for this stat.
+    /// </summary>
+    public float potentialValue;
+
+    /// <summary>
+    /// Just a hand subdivider for the value;
+    /// </summary>
+    public int level;
 }
